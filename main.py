bc78b89f1f22a9584ac45cccca77c3af710da555
Stas Boguta <stasboguta31@gmail.com>
2023-11-04 14:30:31 +0200

    added a console output

--- main.py
+++ main.py
@@ -38,16 +38,17 @@ if __name__ == '__main__':
 
     for i in range(iterations):
         poisson_samples = get_poisson(poison_lam, num_samples)
-        exp_samples = get_exp(exp_lam, num_samples)
+        exp_samples = get_exp(1/exp_lam, num_samples) # numpy exp function accept 1/lam (1/0.5 = 2) as a parameter for generating an exponential distribution with lambda=0.5
         poison_averages.append(calculate_average(poisson_samples))
         exponential_averages.append(calculate_average(exp_samples))
-
+    print(f"Poison: mean {np.mean(poison_averages)}, mode {st.mode(poison_averages)}, median {np.median(poison_averages)}\n")
+    print(f"Exponential: mean {np.mean(exponential_averages)}, mode {st.mode(exponential_averages)}, median {np.median(exponential_averages)}\n")
+    print(f"Current Poison mean values vs theoretical values:\nstd: {np.std(poison_averages)} | {np.sqrt(poison_lam)/np.sqrt(num_samples)}\nmean: {np.mean(poison_averages)} | {poison_lam}\n")
+    print(f"Current Exponential mean values vs theoretical values:\nstd: {np.std(exponential_averages)} | {1/exp_lam / np.sqrt(num_samples)}\nmean: {np.mean(exponential_averages)} | {1/exp_lam}\n")
     plt.figure(figsize=(10, 6))
     plt.subplot(1, 3, 1)
     plt.title("Poison mean distribution")
     plt.hist(poison_averages, bins=100, density=True)
-
-    print(f"mean {np.mean(poison_averages)}, mode {st.mode(poison_averages)}, median {np.median(poison_averages)}")
 
     plt.subplot(1, 3, 2)
     plt.hist(exponential_averages, bins=100, density=True)

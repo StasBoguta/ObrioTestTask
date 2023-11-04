6a903e6032d8b5667b556be63f3bc42cba1d20df
Stas Boguta <stasboguta31@gmail.com>
2023-11-04 19:54:07 +0200

    updated script

--- main.py
+++ main.py
@@ -18,16 +18,6 @@ def get_normal(num_samples):
     return rng.normal(size=num_samples)
 
 
-def calculate_average(samples):
-    return np.mean(samples)
-
-
-def build_hist(samples, name, bins=400):
-    plt.hist(samples, bins, density=True)
-    plt.title(name)
-    plt.show()
-
-
 if __name__ == '__main__':
     poison_lam = 6
     exp_lam = 0.5
@@ -39,12 +29,14 @@ if __name__ == '__main__':
     for i in range(iterations):
         poisson_samples = get_poisson(poison_lam, num_samples)
         exp_samples = get_exp(1/exp_lam, num_samples) # numpy exp function accept 1/lam (1/0.5 = 2) as a parameter for generating an exponential distribution with lambda=0.5
-        poison_averages.append(calculate_average(poisson_samples))
-        exponential_averages.append(calculate_average(exp_samples))
+        poison_averages.append(np.mean(poisson_samples))
+        exponential_averages.append(np.mean(exp_samples))
+
     print(f"Poison: mean {np.mean(poison_averages)}, mode {st.mode(poison_averages)}, median {np.median(poison_averages)}\n")
     print(f"Exponential: mean {np.mean(exponential_averages)}, mode {st.mode(exponential_averages)}, median {np.median(exponential_averages)}\n")
     print(f"Current Poison mean values vs theoretical values:\nstd: {np.std(poison_averages)} | {np.sqrt(poison_lam)/np.sqrt(num_samples)}\nmean: {np.mean(poison_averages)} | {poison_lam}\n")
     print(f"Current Exponential mean values vs theoretical values:\nstd: {np.std(exponential_averages)} | {1/exp_lam / np.sqrt(num_samples)}\nmean: {np.mean(exponential_averages)} | {1/exp_lam}\n")
+
     plt.figure(figsize=(10, 6))
     plt.subplot(1, 3, 1)
     plt.title("Poison mean distribution")
